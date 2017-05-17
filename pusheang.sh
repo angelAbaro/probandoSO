bbaa1b4050be4e374a0cdca6b29583f8160f365b
angelAbaro <angelm.abaro@gmail.com>
2017-05-16 20:00:01 -0500

    subiendo mis proyectos de forma automatica a las 8:00 pm de lunes a Jueves +  el archivo crontab

--- pusheang.sh
+++ pusheang.sh
@@ -2,5 +2,5 @@
 cd /home/abaro/probandoSO
 
 git add .
-git commit -m "subiendo y bajando"
+git commit -m "subiendo mis proyectos de forma automatica a las 8:00 pm de lunes a Jueves"
 git push -u origin master
